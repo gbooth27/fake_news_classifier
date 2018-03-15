c17155899f33b556b8fb776d723afb1ef581d302
grahambooth <gbooth@middlebury.edu>
2018-03-14 23:13:56 -0400

    binary identification of words in text and title

--- parse_data.py
+++ parse_data.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+from collections import OrderedDict
 csv.field_size_limit(sys.maxsize)
 
 
@@ -16,13 +17,16 @@ def parse(filename):
     #Grammar
 
     # Set containing key words
-    FN_figures = {"Russia", "Russian", "WikiLeaks", "Comey", "Donald", "Trump", "Melania", "Jared", "Kushner",
-                      "Putin", "North", "Korea", "Clinton", "Hicks", "Flynn", " Pope", "Francis", "NRA"}
+    FN_figures = {"Russia":0, "Russian":0, "WikiLeaks":0, "Comey":0, "Donald":0, "Trump":0, "Melania":0, "Jared":0, "Kushner":0,
+                      "Putin":0, "North":0, "Korea":0, "Clinton":0, "Hicks":0, "Flynn":0, " Pope":0, "Francis":0, "NRA":0, "!":0,
+                  "Mueller":0, "Hillary":0, "Obama":0, "guns":0, "our":0, "crooked":0, "coal":0}
+    FN_figures = OrderedDict(sorted(FN_figures.items(), key=lambda t: t[0]))
 
     with open(filename, newline='') as infile:
         reader = csv.reader(infile, delimiter=',')
         data = []
         for row in reader:
+            example = []
             if reader.line_num>1:
                 title_data = row[1].split(" ")
                 author_data = row[2]
@@ -34,9 +38,18 @@ def parse(filename):
                     Num_excalmation_points = title.count("!")
                     Num_Question_marks = title.count("?")
                     Fig_det = 0
+
+                    # get binary representations of words
+                    title_dict = OrderedDict()
+                    text_dict = OrderedDict()
+                    for item in FN_figures.keys():
+                        title_dict[item] = FN_figures[item]
+                        text_dict[item] = FN_figures[item]
+
                     for word in title:
                         if word in FN_figures:
                             Fig_det += 1
+                            title_dict[word] = 1
                     # change to fraction of title that are key words
                     Fig_det /= len(title)+1
 
@@ -47,10 +60,14 @@ def parse(filename):
                     #Find Language set
                     if word in FN_figures:
                         Fig_text +=1
+                        text_dict[word]=1
                 Fig_text /= len_text
 
-
-                data.append([Num_Caps, Num_excalmation_points, Num_Question_marks, Fig_det, Fig_text, label])
+                # set up example
+                example.extend(text_dict.values())
+                example.extend( title_dict.values())
+                example.extend([Num_Caps, Num_excalmation_points, Num_Question_marks, Fig_det, Fig_text, label])
+                data.append(example)
     return data
 
 
